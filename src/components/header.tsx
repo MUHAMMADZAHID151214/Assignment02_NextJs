4c819a8e36603d5df25069700f203cf953f5552f
Muhammad Zahid <164073545+MUHAMMADZAHID151214@users.noreply.github.com>
2024-10-06 01:09:14 +0500

    This is a simple intro webpage

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+export default function Header() {
+  return (
+    <div className="container">
+      <div className="header">
+        
+        <ul>
+          <li>Home</li>
+        </ul>
+        <ul>
+          <li>About us</li>
+        </ul>
+        <ul>
+          <li>Contact us</li>
+        </ul>
+        <ul>
+          <li>Education</li>
+        </ul>
+      </div>
+      <div className="intro">
+      <h1>Muhammad Zahid</h1>
+      <h2>BSCS Student PAF-KIET <br/><span> Web3.0, Metaverse & AI Enthusiast</span></h2>
+      </div>
+      <div className="para">
+        <p>
+        Welcome to my personal website! I’m currently pursuing my Bachelor’s in <br />
+        Computer Science at PAF-KIET, where I’m passionate about leveraging <br />
+        technology to create innovative solutions. Alongside my degree, I am <br />
+        enrolled in a specialized course on Web3.0, Metaverse, and AI through <br />
+        the Governor Sindh Initiative, which allows me to explore cutting-edge <br />
+        technologies that shape the future!
+      </p>
+      </div>
+    </div>
+  );
+}
